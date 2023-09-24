f820a8c0212ff9bd490c68cbe6ef84ecfd012bea
buraxta <burakgulluler@gmail.com>
2023-09-24 15:26:51 +0300

    form state and validation with formik and yup

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -3,19 +3,64 @@ import React from "react";
 import AuthFormContainer from "@components/AuthFormContainer";
 import { Button, Input } from "@material-tailwind/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters.")
+    .required("Password is required"),
+});
 
 export default function SignUp() {
+  const {
+    values,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    errors,
+    touched,
+  } = useFormik({
+    initialValues: { name: "", email: "", password: "" },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  console.log(errors);
+
   const formErrors: string[] = [];
 
+  const { name, email, password } = values;
+
   return (
-    <AuthFormContainer title="Create New Account">
-      <Input name="name" label="Name" crossOrigin="anonymous" />
-      <Input name="email" label="Email" crossOrigin="anonymous" />
+    <AuthFormContainer title="Create New Account" onSubmit={handleSubmit}>
+      <Input
+        name="name"
+        label="Name"
+        crossOrigin="anonymous"
+        onChange={handleChange}
+        value={name}
+      />
+      <Input
+        name="email"
+        label="Email"
+        crossOrigin="anonymous"
+        onChange={handleChange}
+        value={email}
+      />
       <Input
         name="password"
         label="Password"
         type="password"
         crossOrigin="anonymous"
+        onChange={handleChange}
+        value={password}
       />
       <Button type="submit" className="w-full">
         Sign up
