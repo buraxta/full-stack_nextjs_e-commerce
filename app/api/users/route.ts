9fa37d633189a0133bf59c72778c96a60a284ec4
buraxta <burakgulluler@gmail.com>
2023-09-26 21:26:37 +0300

    generate verification token

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,14 +1,22 @@
 import startDb from "@/app/lib/db";
+import EmailVerificationToken from "@/app/models/emailVerificationToken";
 import UserModel from "@/app/models/userModel";
 import { NewUserRequest } from "@/app/types";
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import crypto from "crypto";
 
 export const POST = async (req: Request) => {
   const body = (await req.json()) as NewUserRequest;
   await startDb();
   const newUser = await UserModel.create({
     ...body,
+  });
+
+  const token = crypto.randomBytes(32).toString("hex");
+  EmailVerificationToken.create({
+    user: newUser._id,
+    token,
   });
 
   const transport = nodemailer.createTransport({
@@ -20,11 +28,13 @@ export const POST = async (req: Request) => {
     },
   });
 
+  const verificationUrl = `http://localhost:3000/verify?token=${token}&userId=${newUser._id}`;
+
   transport.sendMail({
     from: "burak@mail.com",
     to: newUser.email,
     subject: "Welcome to the app",
-    html: `<h1>Welcome to the app. Please verify your email by clicking on <a href="http://localhost:3000"> this link</a></h1>`,
+    html: `<h1>Welcome to the app. Please verify your email by clicking on <a href="${verificationUrl}"> this link</a></h1>`,
   });
 
   return NextResponse.json(newUser);
